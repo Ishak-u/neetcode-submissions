a1bca4f160505f9489435995ac81ad36a0d7a630
ishak baba shaik <skishakog@gmail.com>
2026-07-30 23:16:02 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        lis1 = list(s)
+        lis2= list(t)
+        lis1.sort()
+        lis2.sort()
+        return lis1 == lis2
